69bae0aa9166fecce4f6c4c558847eb671554ee9
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-03-16 14:41:27 -0700

    add tests! (which are failing)

---
+++ IxlTest/SyntaxTest.hs
@@ -0,0 +1,29 @@
+import Test.Hspec
+import Test.HUnit
+
+import Ixl.Syntax
+
+import Text.ParserCombinators.Parsec (ParseError)
+instance Eq ParseError where
+  x == y = (show x == show y)
+
+baseCommand = Command {
+  c'target = Nothing,
+  c'pipe = Nothing,
+  c'terms = []
+}
+
+term t = Right (Program [baseCommand { c'terms = [t] }])
+
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "interpolation" $ do
+    it "works with a variable" $ do
+      term (Interp [Variable "foo"]) @=?
+        parseIxl "(input)" "\"$foo"
+
+    it "works with a braced variable" $ do
+      term (Interp [Variable "foo"]) @=?
+        parseIxl "(input)" "\"${foo}"
