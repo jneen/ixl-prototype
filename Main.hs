9549b8dc2754e198c56dbf313f6a05f9d115861b
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-12-08 13:42:11 -0800

    restrict readline usage to Main.hs

--- Main.hs
+++ Main.hs
@@ -1,3 +1,34 @@
-import qualified Ixl
+import Ixl (parseIxl)
+import System.Console.Readline (readline, addHistory)
+import System.Posix (queryTerminal)
 
-main = Ixl.main
+makeRepl :: (String -> IO ()) -> String -> IO ()
+makeRepl interp prompt = do
+  line <- readline prompt
+  case line of
+       Nothing -> putStrLn ""
+       Just str -> do
+         addHistory str
+         interp str
+         makeRepl interp prompt
+
+repl :: String -> IO ()
+repl = makeRepl $ \input -> do
+  case parseIxl "(repl)" input of
+       Left error -> print error
+       Right tree -> print tree -- evalIxl tree >>= print
+
+-- runInput = do
+--   c <- getContents
+--   case parseIxl "(stdin)" c of
+--        Left e -> do putStrLn "Error parsing input:"
+--                     print e
+--        Right r -> do
+--          print r
+--          return ()
+
+main = repl ".> "
+--   isatty <- queryTerminal 0
+--   case isatty of
+--        True -> repl ".> "
+--        False -> runInput
